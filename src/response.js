3f5a73b418fa620045c3afc8f489e74ef2f02a41
Dipoydep <kkf227103@gmail.com>
2026-09-09 15:06:06 +0800

    Add CORS headers and response functions

---
+++ src/response.js
@@ -0,0 +1,22 @@
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type"
+};
+
+export function jsonResponse(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      ...corsHeaders,
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  });
+}
+
+export function corsResponse() {
+  return new Response(null, {
+    status: 204,
+    headers: corsHeaders
+  });
+}
